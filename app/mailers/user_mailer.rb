562a91ac70461929ce098e8561f217cdba453b0a
Prudhvi <prudhvikrishna.narra@gmail.com>
2016-01-27 10:49:46 -0500

    sending mail

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome_email.subject
+  #
+  def welcome_email(user)
+    @greeting = "Hi"
+    @name = user.name
+    mail to: user.email
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	after_create :send_welcome_email
+
+	private
+
+	def send_welcome_email
+		UserMailer.welcome_email(self).deliver_now
+	end
+
+end
